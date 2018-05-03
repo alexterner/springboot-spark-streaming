6927871a0c85b0153f2c0faec5381049557afef4
Alex Terner <AlexTerner@gmail.com>
2018-05-03 21:55:48 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/springboot/spark/streaming/springbootsparkstreaming/SpringbootSparkStreamingApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.springboot.spark.streaming.springbootsparkstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSparkStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSparkStreamingApplication.class, args);
+	}
+}

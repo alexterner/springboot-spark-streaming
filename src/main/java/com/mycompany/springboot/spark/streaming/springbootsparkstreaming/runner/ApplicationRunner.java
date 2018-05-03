6d12c8f76337581fcfded5e648c7ef0e16ae768a
Alex Terner <AlexTerner@gmail.com>
2018-05-03 22:38:17 +0300

    Adding java streaming every 2 sec for word count

--- src/main/java/com/mycompany/springboot/spark/streaming/springbootsparkstreaming/runner/ApplicationRunner.java
+++ src/main/java/com/mycompany/springboot/spark/streaming/springbootsparkstreaming/runner/ApplicationRunner.java
@@ -3,13 +3,20 @@ package com.mycompany.springboot.spark.streaming.springbootsparkstreaming.runner
 
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+import scala.Tuple2;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 @Component
@@ -20,16 +27,42 @@ public class ApplicationRunner implements CommandLineRunner{
     private static final Pattern SPACE = Pattern.compile(" ");
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private SparkSession sparkSession;
 
     @Autowired
-    private JavaStreamingContext streamContext;
+    private JavaStreamingContext jssc;
 
     @Autowired
     private JavaSparkContext sc;
 
+    @Value("${streaming.host:localhost}")
+    private String streamingHost;
+
+    @Value("${streaming.port}")
+    private String streamingPort;
+
     @Override
     public void run(String... args) throws Exception {
+
+        // Create a DStream that will connect to hostname:port, like localhost:12345
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream(streamingHost, Integer.valueOf(streamingPort));
+
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+
+        // Count each word in each batch
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
+
+        // Print the first ten elements of each RDD generated in this DStream to the console
+        wordCounts.print();
+
+        jssc.start();              // Start the computation
+        jssc.awaitTermination();   // Wait for the computation to terminate
 
     }
 
